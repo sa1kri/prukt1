00ce029c9e706b530eb59ad597ad3e51be2cd43c
sa1kri <92201506+sa1kri@users.noreply.github.com>
2022-06-04 10:39:45 +0500

    Add files via upload

---
+++ work__the__2/Новая папка (5)/prukt1/Новая папка (2)/3.js
@@ -0,0 +1,260 @@
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  }
+
+b1 = getRandomIntInclusive(1,2)
+b2 = getRandomIntInclusive(1,2)
+b3 = getRandomIntInclusive(1,2)
+b4 = getRandomIntInclusive(1,2)
+b5 = getRandomIntInclusive(1,2)
+b6 = getRandomIntInclusive(1,2)
+b7 = getRandomIntInclusive(1,2)
+b8 = getRandomIntInclusive(1,2)
+b9 = getRandomIntInclusive(1,2)
+b10 = getRandomIntInclusive(1,2)
+b11 = getRandomIntInclusive(1,2)
+b12 = getRandomIntInclusive(1,2)
+b13 = getRandomIntInclusive(1,2)
+b14 = getRandomIntInclusive(1,2)
+b15 = getRandomIntInclusive(1,2)
+b16 = getRandomIntInclusive(1,2)
+b17 = getRandomIntInclusive(1,2)
+b18 = getRandomIntInclusive(1,2)
+
+$("#1").click(function () {
+    if (b1 == 1) {
+        $("#1").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#1").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#2").click(function () {
+    if (b2 == 1) {
+        $("#2").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#2").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#3").click(function () {
+    if (b3 == 1) {
+        $("#3").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#3").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#4").click(function () {
+    if (b4 == 1) {
+        $("#4").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#4").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#5").click(function () {
+    if (b5 == 1) {
+        $("#5").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#5").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#6").click(function () {
+    if (b6 == 1) {
+        $("#6").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#6").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#7").click(function () {
+    if (b7 == 1) {
+        $("#7").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#7").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#8").click(function () {
+    if (b8 == 1) {
+        $("#8").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#8").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#9").click(function () {
+    if (b9 == 1) {
+        $("#9").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#9").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#10").click(function () {
+    if (b10 == 1) {
+        $("#10").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#10").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#11").click(function () {
+    if (b11 == 1) {
+        $("#11").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#11").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#12").click(function () {
+    if (b12 == 1) {
+        $("#12").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#12").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#13").click(function () {
+    if (b13 == 1) {
+        $("#13").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#13").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#14").click(function () {
+    if (b14 == 1) {
+        $("#14").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#14").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#15").click(function () {
+    if (b15 == 1) {
+        $("#15").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#15").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#16").click(function () {
+    if (b16 == 1) {
+        $("#16").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#16").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#17").click(function () {
+    if (b17 == 1) {
+        $("#17").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#17").css({
+            "border": "white"
+        }) 
+    }
+
+})
+$("#18").click(function () {
+    if (b18 == 1) {
+        $("#18").css({
+            "color": "red",
+            "border": "white"
+        }) 
+    } else {
+        $("#18").css({
+            "border": "white"
+        }) 
+    }
+
+})
+
